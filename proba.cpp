fc6f4798fc05301015683f8c38df433321480969
kerim-kovac <nejlakerim455@gmail.com>
2026-04-01 13:57:28 +0200

    test-tri

--- proba.cpp
+++ proba.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
+#include<vector>
+
+using namespace std;
+
+template<typename T>
+T zamjena(T &a, T &b){
+     T pomocna = a;
+     a = b;
+     b = pomocna;
+     cout<<"\nNakon zamjene\n";
+     cout<<"A: "<<a<<"\n";
+     cout<<"B: "<<b<<"\n";
+}
+
 
 int main(){
-
-     std::cout<<"TEST\n";
-     std::cout<<"TEST-2";
-     return 0;
+     int a=5;
+     int b=7;
+     cout<<"\nPrije zamjene\n";
+     cout<<"A: "<<a<<"\n";
+     cout<<"B: "<<b<<"\n";
+     zamjena(a,b);
 }
