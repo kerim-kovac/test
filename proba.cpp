35beee4e8825087f558928ad770ea1e0fc6fde68
kerim-kovac <nejlakerim455@gmail.com>
2026-04-04 12:02:03 +0200

    aaaaa

--- proba.cpp
+++ proba.cpp
@@ -2,23 +2,39 @@
 #include<vector>
 
 using namespace std;
-
 template<typename T>
-T zamjena(T &a, T &b){
-     T pomocna = a;
-     a = b;
-     b = pomocna;
-     cout<<"\nNakon zamjene\n";
-     cout<<"A: "<<a<<"\n";
-     cout<<"B: "<<b<<"\n";
+void zamjeniMinMax(T* pocetak, T* kraj, T* &pokN, T* &pokV){
+     T miminalna = *pocetak;
+     T maximalna = *pocetak;
+     pokN = pocetak;
+     pokV = pocetak;
+     for(T* pok=pocetak;pok<kraj;pok++){
+          if(*pok>maximalna){
+               maximalna = *pok;
+               pokV = pok;
+          }else if(*pok<miminalna){
+               miminalna = *pok;
+               pokN = pok;
+          }
+     }
+     cout<<"\nPRIJE ZAMJENE\n";
+     cout<<" najmanja vrijednost: "<<*pokN<<endl;
+     cout<<" najveca vrijednost "<<*pokV<<endl;
+     cout<<"\n-------------------\n";
+     T temp = *pokN;
+     *pokN = *pokV;
+     *pokV = temp;
+     cout<<"\NAKON ZAMJENE\n";
+     cout<<"najmanja vrijednost: "<<*pokN<<endl;
+     cout<<"najveca vrijednost "<<*pokV<<endl;
+     cout<<"\n-------------------\n";
 }
 
 
 int main(){
-     int a=5;
-     int b=7;
-     cout<<"\nPrije zamjene\n";
-     cout<<"A: "<<a<<"\n";
-     cout<<"B: "<<b<<"\n";
-     zamjena(a,b);
+     int niz[]{1,2,3};
+     int* pokNajm(nullptr);
+     int* pokNajv(nullptr);
+     zamjeniMinMax(niz,niz+3,pokNajm,pokNajv);
+     return 0;
 }
